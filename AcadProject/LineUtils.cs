e97b9d2812c8e11b5bbba76def425a98c7b82feb
Tuan Vu <tuanvvk53.hust@gmail.com>
2017-01-26 21:55:44 +0700

    Initializing acad project
    Initializing acad project

---
+++ AcadProject/LineUtils.cs
@@ -0,0 +1,98 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Windows;
+using System.IO;
+using System.Text;
+using System;
+
+namespace AcadProjectLineUtils
+{
+    public class LineUtils
+    {
+        //Delta data
+        public double DELTA_POINT = 0.0;
+
+
+        /* Convert polyline to lines
+         * and remove duplicated line
+         */
+        public List<Line> convertPolyLineToLine(List<Polyline> mPolylineCollection, Editor ed)
+        {
+            List<Line> result = new List<Line>();
+            for (int i = 0; i < mPolylineCollection.Count; i++)
+            {
+                int nov = mPolylineCollection[i].NumberOfVertices;
+                for (int vid = 0; vid < nov - 1; vid++)
+                {
+                    if (result.Count > 0 && isCheckLineDuplicate(result, new Line(mPolylineCollection[i].GetPoint3dAt(vid), mPolylineCollection[i].GetPoint3dAt(vid + 1))))
+                        ed.WriteMessage("Line is duplicated or should be removed!! Ignore it\n");
+                    else
+                        result.Add(new Line(mPolylineCollection[i].GetPoint3dAt(vid), mPolylineCollection[i].GetPoint3dAt(vid + 1)));
+
+                }
+            }
+            return result;
+        }
+
+        /*
+         * Check duplicated line
+         * A line already in collection or not
+         * Return true/false
+         */
+        public bool isCheckLineDuplicate(List<Line> mLineCollection, Line line)
+        {
+            for (int i = 0; i < mLineCollection.Count; i++)
+            {
+                if (isLineBeRemoved(line) || line.Equals(mLineCollection[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        /*
+         * Remove vertical line
+         * or Line has angle equals 90
+         */
+        public bool isLineBeRemoved(Line line) 
+        {
+            if (line.StartPoint.X == line.EndPoint.X)
+                return true;
+            return false;
+        }
+
+        /*
+         * Dump debug information to txt file
+         */
+        public void dump(String dumpData, Editor ed)
+        {
+            var txt = "D:/linedata.txt";
+            //Write content to TXT file
+            if (!String.IsNullOrEmpty(dumpData))
+            {
+                try
+                {
+                    // Write the contents to the selected TXT file
+
+                    using (
+                      var sw = new StreamWriter(txt, false, Encoding.UTF8)
+                    )
+                    {
+                        sw.WriteLine(dumpData);
+                    }
+                }
+                catch (System.IO.IOException)
+                {
+                    // We might have an exception, if the TXT is open in
+                    // Excel, for instance... could also show a messagebox
+                    ed.WriteMessage("\nUnable to write to file.");
+                }
+            }
+
+        }
+    }
+}
---
+++ AcadProject/MainClass.cs
@@ -0,0 +1,112 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Windows;
+using System.IO;
+using System.Text;
+using System;
+using AcadProjectLineUtils;
+
+namespace AcadProject
+{
+
+    public class Commands
+    {
+
+        //Collections of LINEs/POLYLINEs/DBText
+        List<Line> mLineCollection = new List<Line>();
+        List<Polyline> mPolylineCollection = new List<Polyline>();
+        List<DBText> mDBTextCollection = new List<DBText>();
+
+        //Collections of LAYERs
+        List<String> mLayerCollection = new List<String>();
+
+
+        //LINE UTILS OBJECTs
+        LineUtils mLineUtilObject = new LineUtils();
+
+
+        [CommandMethod("DST", CommandFlags.UsePickSet)]
+        public void SelectAllLines()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
+            var ed = doc.Editor;
+
+            //CLEAR DATA
+            mDBTextCollection.Clear();
+            mLayerCollection.Clear();
+            mPolylineCollection.Clear();
+            mLineCollection.Clear();
+
+
+            PromptSelectionOptions pso = new PromptSelectionOptions();
+
+            //Message to user for selction
+            pso.MessageForAdding = "\nSelect area contains objects: ";
+            //Remove duplicated selection
+            pso.AllowDuplicates = false;
+
+            // Perform our restricted selection
+            PromptSelectionResult psr = ed.GetSelection(pso);
+
+            if (psr.Status != PromptStatus.OK)
+                return;
+
+            //Dump debug information to txt file
+            var sb = new StringBuilder();
+
+            //Open a transaction
+            Transaction tr = doc.TransactionManager.StartTransaction();
+            foreach (ObjectId acSSObj in psr.Value.GetObjectIds())
+            {
+                //get properties
+                Entity ent = (Entity)tr.GetObject(acSSObj, OpenMode.ForRead);
+
+                //CHECKING OBJECT TYPE
+                switch (ent.GetType().ToString())
+                {
+
+                    case "Autodesk.AutoCAD.DatabaseServices.Line":
+                        Line ln = tr.GetObject(acSSObj, OpenMode.ForRead) as Line;
+                        mLineCollection.Add(ln);
+                        break;
+
+                    case "Autodesk.AutoCAD.DatabaseServices.Polyline":
+                        Polyline pln = tr.GetObject(acSSObj, OpenMode.ForRead) as Polyline;
+                        mPolylineCollection.Add(pln);
+                        break;
+
+                    case "Autodesk.AutoCAD.DatabaseServices.DBText":
+                        DBText dbt = tr.GetObject(acSSObj, OpenMode.ForRead) as DBText;
+                        mDBTextCollection.Add(dbt);
+                        break;
+
+                    default:
+                        ed.WriteMessage("\nNo handle");
+                        ed.WriteMessage("\nType:         " + ent.GetType().ToString());
+                        break;
+                }
+
+                ent.Dispose();
+            }
+            //Apply changes and close transaction
+            tr.Commit();
+
+            mLineCollection.AddRange(mLineUtilObject.convertPolyLineToLine(mPolylineCollection, ed));
+            sb.Append("Number of line : " + mLineCollection.Count + "\n");
+            for (int i = 0; i < mLineCollection.Count; i++)
+            {
+                sb.Append("\nLine " + (i + 1) + " : " + mLineCollection[i].StartPoint.X + "  " + mLineCollection[i].StartPoint.Y);
+                sb.Append("  " + mLineCollection[i].EndPoint.X + "  " + mLineCollection[i].EndPoint.Y + "\n");
+
+            }
+            mLineUtilObject.dump(sb.ToString(), ed);
+        }
+
+    }
+}
